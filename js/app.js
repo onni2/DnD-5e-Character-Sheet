34ee4ae9eb23191d144b99732181d4f6eb953b87
Michael <not@needed.com>
2021-06-07 13:14:18 -0400

    Removed not needed JS code

--- js/app.js
+++ js/app.js
@@ -1,12 +1,3 @@
-function openPage(pageName) {
-    var i;
-    var x = document.getElementsByClassName("page");
-    for (i = 0; i < x.length; i++) {
-        x[i].style.display = "none";
-    }
-    document.getElementById(pageName).style.display = "";
-}
-
 function w3_open() {
     document.getElementById("mySidebar").style.display = "block";
 }
